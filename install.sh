0f44074340c2def83f7a8c2aa5d35a5f9343f77d
Alexander Golovanov <Golovanov12345@gmail.com>
2018-05-02 00:54:05 +0300

    fix something

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ echo "" >> ~/.zshrc
 
 echo "Installing Oh-My-Zsh..."
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O zsh_install.sh)"
-sed "s|~|/home/$(whoami)|g" -i zsh_install.sh
+sed "s|~|/home/$SUDO_USER|g" -i zsh_install.sh
 sh zsh_install.sh
 rm zsh_install.sh
 
